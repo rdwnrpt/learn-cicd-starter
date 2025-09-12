04970de5b8ce186e528cbf36d489e3086ff68da1
rdwnrpt <ridwan.rputra@gmail.com>
2025-09-12 14:11:52 +0700

    add fix

--- main.go
+++ main.go
@@ -88,9 +88,11 @@ func main() {
 	v1Router.Get("/healthz", handlerReadiness)
 
 	router.Mount("/v1", v1Router)
+
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: time.Second * 5,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
